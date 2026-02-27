8351591a74f187248bf4fbb9417289ccbcc6f368
MD <degiorgiomatthew95@gmail.com>
2026-02-27 18:55:57 +0100

    Initial Commit

---
+++ LedgerVault/LedgerVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerVaultApp.swift
+//  LedgerVault
+//
+//  Created by Matthew on 27/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct LedgerVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
